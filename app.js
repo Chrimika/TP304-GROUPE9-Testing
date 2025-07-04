93bb4dc96175b9e6126ee85c23cf78d5cea8127c
Chrimika <mbachristian58@gmail.com>
2025-07-04 18:43:10 +0100

    initialisation du projet, Docker,Mongo,Express et mise en place de app.js-le server

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-// server.js
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const PORT = 3000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
 
 // Route de test
 app.get('/', (req, res) => {
-  res.send('Backend prêt pour le TP !');
+  res.send('Vehicle Service API is running!');
 });
 
 // Démarrer le serveur
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
